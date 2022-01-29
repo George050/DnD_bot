9a2a80cd4405ea8e4a84041567b20011ea8dae9e
George050 <IStormer8@gmail.com>
2022-01-30 01:27:21 +0500

    First change

--- main code.py
+++ main code.py
@@ -6,15 +6,15 @@ bot = telebot.TeleBot('5226221353:AAHIkDyNlZEGVuB6C76w9Iqp9prPYl72HH8')
 
 @bot.message_handler(commands=["start", "help"])
 def start(m, res=False):
-    bot.send_message(m.chat.id, 'Мы можем отправить вам основную документацию. Для этого напишите:'
-                                'книга игрока'
-                                'руководство мастера'
-                                'бестиарий'
-                                'справочник по монстрам')
+    bot.send_message(m.chat.id, 'Мы можем отправить вам основную документацию. Для этого напишите: \n'
+                                '-книга игрока \n'
+                                '-руководство мастера \n'
+                                '-бестиарий \n'
+                                '-справочник по монстрам \n')
 
 @bot.message_handler(content_types=["text"])
 def handle_text(message):
 
-    bot.send_message(message.chat.id, 'Вы написали: ' + message.text)
+    bot.send_message(message.chat.id, 'Я неготов, поэтому выведу вам это: \n Вы написали: ' + message.text)
 
 bot.polling(none_stop=True, interval=0)
